2c6f0645200fb3d95512d658f563ec810e1df054
Jeremy Danner <jeremydanner2@gmail.com>
2017-04-20 18:56:04 +0200

    added .editorconfig

--- public/themes/mashallahgruppen/header.php
+++ public/themes/mashallahgruppen/header.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
 
-      <meta name="author" content="HandelsMarketing">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-      <title>Site Title</title>
-  </head>
+    <meta name="author" content="HandelsMarketing">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <title>Site Title</title>
+</head>
 <body>
+
+    <div class="container">
