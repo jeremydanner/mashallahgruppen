9c8b642f62aa9dea5ce661da5deb5920b7b098c8
Jeremy Danner <jeremydanner2@gmail.com>
2017-04-22 12:22:20 +0200

    minor changes to the navbar

--- public/themes/mashallahgruppen/header.php
+++ public/themes/mashallahgruppen/header.php
@@ -12,4 +12,24 @@
 </head>
 <body>
 
-    <div class="container body">
+    <!-- Bootsrap 4 navbar -->
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded" style="background-color: #e42407;">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="/">Mashallahgruppen IMG</a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <?php foreach (get_pages() as $page): ?>
+                    <li class="nav-item <?php if (is_page($page)) { echo 'active'; } ?>">
+                        <a class="nav-link" href="<?php echo get_permalink($page); ?>">
+                            <?php echo $page->post_title; ?>
+                        </a><!-- /nav-link -->
+                    </li><!-- /nav-item -->
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
