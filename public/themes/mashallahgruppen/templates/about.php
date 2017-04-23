3e8f33d248fe15e22a683ae2f171961bf2a981d2
Jeremy Danner <jeremydanner2@gmail.com>
2017-04-23 18:04:51 +0200

    basics for about page done

--- public/themes/mashallahgruppen/templates/about.php
+++ public/themes/mashallahgruppen/templates/about.php
@@ -13,6 +13,49 @@ Template name: About
 
 <?php get_header(); ?>
 
-<h1>about</h1>
+<div class="headerHero">
+    <img class="hero" src="<?php the_field('hero'); ?>" />
+</div>
+
+<div class="container">
+
+    <div class="rowWrapper">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="colInnerWrapper">
+                    <h1 class="left"><?php the_field('mainTitle'); ?></h1>
+
+                    <?php the_field('mainBody'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="rowWrapper">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="colInnerWrapper">
+                    <h2><?php the_field('subTitle'); ?></h2>
+
+                    <?php the_field('subBody'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="rowWrapper">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="colInnerWrapper">
+                    <h2><?php the_field('subTitle2'); ?></h2>
+
+                    <?php the_field('subBody2'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</div><!-- End Container -->
 
 <?php get_footer();
