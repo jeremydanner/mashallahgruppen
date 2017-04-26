3747902b98aee5ffe88fcef686e15ec2d1c03045
Jeremy Danner <jeremydanner2@gmail.com>
2017-04-26 16:40:09 +0200

    minor fixes

--- public/themes/mashallahgruppen/templates/fieldOfWork.php
+++ public/themes/mashallahgruppen/templates/fieldOfWork.php
@@ -26,25 +26,19 @@ Template name: FieldOfWork
         </div>
     </div>
 
-    <div class="latestNewsWrapper">
-            <?php if (have_rows('FieldOfWorkBody')): ?>
-                <?php while (have_rows('FieldOfWorkBody')): the_row(); ?>
-                    <div class="row">
-                    <div class="rowWrapper">
-                        <div class="col-md-12">
-                            <div class="colInnerWrapper">
-                                <div class="col-md-6 latestNews">
-                                    <img src="<?php the_sub_field('bodyImage') ?>" alt="<?php the_sub_field('bodyImageTitle') ?>">
-                                    <p><?php the_sub_field('body') ?></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                <?php endwhile; ?>
-            <?php endif; ?>
+    <div class="row">
 
-    </div>
+    <?php if (have_rows('FieldOfWorkBody')): ?>
+        <?php while (have_rows('FieldOfWorkBody')): the_row(); ?>
+            <div class="col-md-6 fields">
+                <img src="<?php the_sub_field('bodyImage') ?>" alt="<?php the_sub_field('bodyImageTitle') ?>">
+                <p><?php the_sub_field('body') ?></p>
+            </div>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
+
+</div>
 
 </div><!-- End Container -->
 
