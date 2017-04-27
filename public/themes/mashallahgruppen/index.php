45c38e30d6bcdc7ee541ed2192ee27b19cdee0f1
Jeremy Danner <jeremydanner2@gmail.com>
2017-04-27 21:18:20 +0200

    minor changes

--- public/themes/mashallahgruppen/index.php
+++ public/themes/mashallahgruppen/index.php
@@ -41,7 +41,7 @@ Template name: Home
         <div class="circle"></div>
     </div>
 
-    <div class="latestNewsWrapper">
+    <!-- <div class="latestNewsWrapper">
         <h2>Senaste nytt</h2>
         <div class="row">
             <div class="col-md-4 latestNews">
@@ -56,8 +56,14 @@ Template name: Home
                 <img src="https://scontent-frt3-1.xx.fbcdn.net/v/t31.0-8/s960x960/17918029_783359525163310_8320188407829282925_o.jpg?oh=6cf6dde5e71956e5aa527ef9d145e22e&oe=597D0C14" alt="">
             </div>
         </div>
+    </div> -->
+    <!-- SnapWidget -->
+    <div class="row">
+        <div class="col-md-12">
+            <iframe src="https://snapwidget.com/embed/373963" class="snapwidget-widget" allowTransparency="true" frameborder="0" scrolling="no" style="border:none; overflow:hidden; width:100%; "></iframe>
+        </div>
     </div>
 
 </div><!-- End Container -->
-
+<script src="https://snapwidget.com/js/snapwidget.js"></script>
 <?php get_footer();
