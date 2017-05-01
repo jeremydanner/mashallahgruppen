a48d0ef92e1ec87f60e5ba1102fa22080d9c6213
Jeremy Danner <jeremydanner2@gmail.com>
2017-05-01 11:13:01 +0200

    fixed footer

--- public/themes/mashallahgruppen/footer.php
+++ public/themes/mashallahgruppen/footer.php
@@ -9,7 +9,11 @@
 <div class="row footerWrapper">
 
     <div class="col-md-4 footer">
-        <span class="footerLeft"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d532.5953491834321!2d12.011739029291736!3d57.72696491490931!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464ff4641abeeaa9%3A0x90a7abc123878fb!2sByfogdegatan+6C%2C+415+05+G%C3%B6teborg!5e0!3m2!1sen!2sse!4v1493317993779" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe></span>
+        <span class="footerLeft">
+            <p>Följ oss på</p>
+            <a href="https://www.facebook.com/mashallahgruppen/"><img src="<?= get_template_directory_uri(); ?>/public/icons/facebook-logo.svg" alt="facebook logo"></a>
+            <a href="https://www.instagram.com/mashallahgruppen/"><img src="<?= get_template_directory_uri(); ?>/public/icons/instagram-social-network-logo-of-photo-camera.svg" alt="instagram logo"></a>
+        </span>
     </div>
 
     <div class="col-md-4 footer">
